5062aedec3c6889fffc70231ed9809c731ae6526
Hamza Mughal <hm588940@gmail.com>
2022-11-25 18:35:16 +0500

    add

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+        <div>
+            <h1>Techstar</h1>
+            <p>@all right reserved</p>
+        </div>
+        <div>
+            <h5>Follow Us</h5>
+            <div>
+                <h6> Youtube</h6>
+                <h6> Instagram</h6>
+                <h6>Facebook</h6>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
